28e5c6cf3bdfbf943577f9655a8bd7f01266e21a
twilight <240590367@qq.com>
2020-09-02 21:12:44 +0800

    初次提交以作备份

---
+++ config.py
@@ -0,0 +1,17 @@
+# num_layers = 4
+# d_model = 128
+# dff = 512
+num_heads = 8
+
+num_layers = 6
+d_model = 512
+dff = 2048
+
+
+BUFFER_SIZE = 20000
+BATCH_SIZE = 64
+
+MAX_LENGTH = 40
+
+EPOCHS = 20
+dropout_rate = 0.1
---
+++ main.py
@@ -0,0 +1,215 @@
+from model.utils import CustomSchedule, create_masks
+from config import *
+import tensorflow as tf
+from model.transformer import Transformer
+from data_prepare import input_vocab_size, target_vocab_size, train_dataset, tokenizer_en, tokenizer_pt
+import time
+import matplotlib.pyplot as plt
+
+
+def loss_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    loss_ = loss_object(real, pred)
+
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+
+    return tf.reduce_mean(loss_)
+
+
+learning_rate = CustomSchedule(d_model)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
+                                     epsilon=1e-9)
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+    name='train_accuracy')
+
+
+def train():
+    train_step_signature = [
+        tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+        tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+    ]
+
+    @tf.function(input_signature=train_step_signature)
+    def train_step(inp, tar):
+        tar_inp = tar[:, :-1]
+        tar_real = tar[:, 1:]
+
+        enc_padding_mask, combined_mask, dec_padding_mask = create_masks(inp, tar_inp)
+
+        with tf.GradientTape() as tape:
+            predictions, _ = transformer(inp, tar_inp,
+                                         True,
+                                         enc_padding_mask,
+                                         combined_mask,
+                                         dec_padding_mask)
+            loss = loss_function(tar_real, predictions)
+
+        gradients = tape.gradient(loss, transformer.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+
+        train_loss(loss)
+        train_accuracy(tar_real, predictions)
+
+    transformer = Transformer(num_layers, d_model, num_heads, dff,
+                              input_vocab_size, target_vocab_size,
+                              pe_input=input_vocab_size,
+                              pe_target=target_vocab_size,
+                              rate=dropout_rate)
+
+    checkpoint_path = "./checkpoints/train"
+
+    ckpt = tf.train.Checkpoint(transformer=transformer,
+                               optimizer=optimizer)
+
+    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+    # 如果检查点存在，则恢复最新的检查点。
+    if ckpt_manager.latest_checkpoint:
+        ckpt.restore(ckpt_manager.latest_checkpoint)
+        print('Latest checkpoint restored!!')
+
+    for epoch in range(EPOCHS):
+        start = time.time()
+
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+
+        # inp -> portuguese, tar -> english
+        for (batch, (inp, tar)) in enumerate(train_dataset):
+            train_step(inp, tar)
+
+            if batch % 50 == 0:
+                print('Epoch {} Batch {} Loss {:.4f} Accuracy {:.4f}'.format(
+                    epoch + 1, batch, train_loss.result(), train_accuracy.result()))
+
+        if (epoch + 1) % 5 == 0:
+            ckpt_save_path = ckpt_manager.save()
+            print('Saving checkpoint for epoch {} at {}'.format(epoch + 1,
+                                                                ckpt_save_path))
+
+        print('Epoch {} Loss {:.4f} Accuracy {:.4f}'.format(epoch + 1,
+                                                            train_loss.result(),
+                                                            train_accuracy.result()))
+
+        print('Time taken for 1 epoch: {} secs\n'.format(time.time() - start))
+
+
+def evaluate(inp_sentence):
+    start_token = [tokenizer_pt.vocab_size]
+    end_token = [tokenizer_pt.vocab_size + 1]
+
+    # 输入语句是葡萄牙语，增加开始和结束标记
+    inp_sentence = start_token + tokenizer_pt.encode(inp_sentence) + end_token
+    encoder_input = tf.expand_dims(inp_sentence, 0)
+
+    # 因为目标是英语，输入 transformer 的第一个词应该是
+    # 英语的开始标记。
+    decoder_input = [tokenizer_en.vocab_size]
+    output = tf.expand_dims(decoder_input, 0)
+
+    transformer = Transformer(num_layers, d_model, num_heads, dff,
+                              input_vocab_size, target_vocab_size,
+                              pe_input=input_vocab_size,
+                              pe_target=target_vocab_size,
+                              rate=dropout_rate)
+    checkpoint_path = "./checkpoints/train"
+
+    ckpt = tf.train.Checkpoint(transformer=transformer, optimizer=optimizer)
+
+    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+    # 如果检查点存在，则恢复最新的检查点。
+    if ckpt_manager.latest_checkpoint:
+        ckpt.restore(ckpt_manager.latest_checkpoint)
+        print('Latest checkpoint restored!!')
+
+    for i in range(MAX_LENGTH):
+        enc_padding_mask, combined_mask, dec_padding_mask = create_masks(
+            encoder_input, output)
+
+        # predictions.shape == (batch_size, seq_len, vocab_size)
+        predictions, attention_weights = transformer(encoder_input, output, False, enc_padding_mask, combined_mask,
+                                                     dec_padding_mask)
+
+        # 从 seq_len 维度选择最后一个词
+        predictions = predictions[:, -1:, :]  # (batch_size, 1, vocab_size)
+
+        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+        # 如果 predicted_id 等于结束标记，就返回结果
+        if predicted_id == tokenizer_en.vocab_size + 1:
+            return tf.squeeze(output, axis=0), attention_weights
+
+        # 连接 predicted_id 与输出，作为解码器的输入传递到解码器。
+        output = tf.concat([output, predicted_id], axis=-1)
+
+    return tf.squeeze(output, axis=0), attention_weights
+
+
+def plot_attention_weights(attention, sentence, result, layer):
+    fig = plt.figure(figsize=(16, 8))
+
+    sentence = tokenizer_pt.encode(sentence)
+
+    attention = tf.squeeze(attention[layer], axis=0)
+
+    for head in range(attention.shape[0]):
+        ax = fig.add_subplot(2, 4, head + 1)
+
+        # 画出注意力权重
+        ax.matshow(attention[head][:-1, :], cmap='viridis')
+
+        fontdict = {'fontsize': 10}
+
+        ax.set_xticks(range(len(sentence) + 2))
+        ax.set_yticks(range(len(result)))
+
+        ax.set_ylim(len(result) - 1.5, -0.5)
+
+        ax.set_xticklabels(
+            ['<start>'] + [tokenizer_pt.decode([i]) for i in sentence] + ['<end>'],
+            fontdict=fontdict, rotation=90)
+
+        ax.set_yticklabels([tokenizer_en.decode([i]) for i in result
+                            if i < tokenizer_en.vocab_size],
+                           fontdict=fontdict)
+
+        ax.set_xlabel('Head {}'.format(head + 1))
+
+    plt.tight_layout()
+    plt.show()
+
+
+def translate(sentence, plot=''):
+    result, attention_weights = evaluate(sentence)
+
+    predicted_sentence = tokenizer_en.decode([i for i in result
+                                              if i < tokenizer_en.vocab_size])
+
+    print('Input: {}'.format(sentence))
+    print('Predicted translation: {}'.format(predicted_sentence))
+
+    if plot:
+        plot_attention_weights(attention_weights, sentence, result, plot)
+
+
+def test_translate():
+    translate("este é um problema que temos que resolver.")
+    print("Real translation: this is a problem we have to solve .")
+
+    translate("os meus vizinhos ouviram sobre esta ideia.")
+    print("Real translation: and my neighboring homes heard about this idea .")
+
+    translate(
+        "vou então muito rapidamente partilhar convosco algumas histórias de algumas coisas mágicas que aconteceram.")
+    print(
+        "Real translation: so i 'll just share with you some stories very quickly of some magical things that have happened .")
+
+    translate("este é o primeiro livro que eu fiz.", plot='decoder_layer4_block2')
+    print("Real translation: this is the first book i've ever done.")
